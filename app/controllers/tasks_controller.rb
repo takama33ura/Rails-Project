003dfc22dbedb8203e9892310abc62e5ac6c3600
jiro.kirameki <takama33ura@gmail.com>
2017-04-23 14:15:56 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
@@ -48,8 +50,10 @@ class TasksController < ApplicationController
   end
 
   private
-
+  def set_task
+    @task = Task.find(params[:id])
+  end
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)  
-end
+    params.require(:task).permit(:content, :status)  
+  end
